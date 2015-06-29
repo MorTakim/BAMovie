9acc71a85d94faebec4388516c19db398c7f921c
ysBayram <mirza41@hotmail.com>
2015-06-29 14:30:24 +0300

    UI admin smthg

---
+++ BilgeMVC/Controllers/FilmController.cs
@@ -0,0 +1,123 @@
+﻿using ProjectTemplate.Core.Abstractions.Service;
+using ProjectTemplate.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BilgeMVC.Controllers
+{
+    public class FilmController : BaseController
+    {
+        private readonly IServiceFilm _serviceFilm;
+
+        public FilmController(IServiceFilm serviceFilm)
+        {
+            _serviceFilm = serviceFilm;
+        }
+
+        public PartialViewResult Films()
+        {
+            return PartialView("Partial/_Films", _serviceFilm.GetAll());
+        }
+
+        public PartialViewResult FilmDetail(int id)
+        {
+            Film flm = new Film();
+            flm = _serviceFilm.GetById(id);
+            flm.Actors = _serviceFilm.GetById(id).Actors.ToList();
+            flm.Directors = _serviceFilm.GetById(id).Directors.ToList();
+            flm.Writers = _serviceFilm.GetById(id).Writers.ToList();
+            flm.Producers = _serviceFilm.GetById(id).Producers.ToList();
+            flm.FilmTypes = _serviceFilm.GetById(id).FilmTypes.ToList();
+
+            return PartialView("Partial/_FilmDetail", flm);
+        }
+
+
+        public ActionResult FilmAdd()
+        {
+            if (Session["account"] != null)
+            {
+                return View();
+            }
+            else
+                return RedirectToAction("Login", "User");
+        }
+
+        [HttpPost]
+        public ActionResult FilmAdd(Film film, string txtPublishDate, HttpPostedFileBase File)
+        {
+            if (Session["account"] != null)
+            {
+                DateTime dt = Convert.ToDateTime(txtPublishDate);
+
+                film.PublishDate = dt;
+
+                if (File != null)
+                {
+                    File.SaveAs(Server.MapPath("~/FilmPosters/") + File.FileName);
+                    film.PosterImgPath = "/FilmPosters/" + File.FileName;
+                }
+
+                _serviceFilm.Insert(film);
+
+                return RedirectToAction("FilmListele", "Film");
+            }
+            else
+                return RedirectToAction("Login", "User");
+        }
+
+
+        public ActionResult FilmEdit(int id)
+        {
+            if (Session["account"] != null)
+            {
+                return View(_serviceFilm.GetById(id));
+            }
+            else
+                return RedirectToAction("Login", "User");
+        }
+
+        [HttpPost]
+        public ActionResult FilmEdit(Film film)
+        {
+            if (Session["account"] != null)
+            {
+                if (ModelState.IsValid)
+                {
+                    Film guncellenenFilm = _serviceFilm.GetById(film.Id);
+                    guncellenenFilm.Name = film.Name;
+                    guncellenenFilm.PublishDate = film.PublishDate;
+                    guncellenenFilm.ImdbPoint = film.ImdbPoint;
+                    guncellenenFilm.Website = film.Website;
+
+                    _serviceFilm.Update(guncellenenFilm);
+                }
+
+                return RedirectToAction("FilmListele", "Film");
+            }
+            else
+                return RedirectToAction("Login", "User");
+        }
+
+        public ActionResult FilmDelete(int id)
+        {
+            if (Session["account"] != null)
+            {
+                _serviceFilm.Delete(_serviceFilm.GetById(id));
+                return RedirectToAction("FilmListele", "Film");
+
+            }
+            else
+                return RedirectToAction("Login", "User");
+        }
+
+        public ActionResult FilmListele()
+        {
+            return View(_serviceFilm.GetAll());
+        }
+
+    }
+}
---
+++ BilgeMVC/Controllers/UserController.cs
@@ -0,0 +1,134 @@
+﻿using ProjectTemplate.Core.Abstractions.Service;
+using ProjectTemplate.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjectTemplate.Core.Enums;
+
+namespace BilgeMVC.Controllers
+{
+    public class UserController : BaseController
+    {
+        private readonly IServiceUser _serviceUser;
+
+        public UserController(IServiceUser serviceUser)
+        {
+            _serviceUser = serviceUser;
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string eMail, string password)
+        {
+            var result = _serviceUser.LoginControl(eMail, password);
+            if (result != null)
+            {
+                Session.Add("account", result);
+                return RedirectToAction("Admin", "User");
+            }
+            else
+            {
+                ViewBag.Message = "Geçersiz kullanıcı adı ya da şifre!";
+                return View();
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            Session["account"] = null;
+            return RedirectToAction("Index", "Base");
+        }
+
+        public ActionResult Admin()
+        {
+            if (Session["account"] != null)
+            {
+                return View(_serviceUser.GetAll());
+            }
+            else
+                return RedirectToAction("Login", "User");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            if (Session["account"] != null)
+            {
+                return View(_serviceUser.GetById(id));
+            }
+            else
+                return RedirectToAction("Login", "User");
+        }
+
+        [HttpPost]
+        public ActionResult Edit(User user)
+        {
+            if (Session["account"] != null)
+            {
+                if (ModelState.IsValid)
+                {
+                    User updated = _serviceUser.GetById(user.Id);
+                    updated.Name = user.Name;
+                    updated.Surname = user.Surname;
+                    updated.Email = user.Email;
+                    _serviceUser.Update(updated);
+                }
+                return RedirectToAction("Admin", "User");
+            }
+            else
+                return RedirectToAction("Login", "User");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            if (Session["account"] != null)
+            {
+                _serviceUser.Delete(_serviceUser.GetById(id));
+                return RedirectToAction("Admin", "User");
+
+            }
+            else
+                return RedirectToAction("Login", "User");
+        }
+
+        public ActionResult Add()
+        {
+            if (Session["account"] != null)
+            {
+                return View();
+            }
+            else
+                return RedirectToAction("Login", "User");
+        }
+
+        [HttpPost]
+        public ActionResult Add(User user, string birthdate, HttpPostedFileBase userimg)
+        {
+            if (Session["account"] != null)
+            {
+                if (_serviceUser.GetAll().FirstOrDefault(s => s.Email.ToUpper() == user.Email.ToUpper()) != null)
+                    return RedirectToAction("Add");
+
+                if (userimg != null)
+                {
+                    userimg.SaveAs(Server.MapPath("~/img/user/") + userimg.FileName);
+                    user.ImagePath = "~/img/user/" + userimg.FileName;
+                }
+
+                user.Password = user.Password;
+                user.BirthDate = Convert.ToDateTime(birthdate);
+                user.Role = Role.User;
+                _serviceUser.Insert(user);
+                return RedirectToAction("Admin", "User");
+            }
+            else
+                return RedirectToAction("Login", "User");
+        }
+
+    }
+}
